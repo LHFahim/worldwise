ca98e7e0c092b4bfdefd8d0b5c4d6a36a3bf8858
LHFahim <fahimlutful@gmail.com>
2023-09-19 13:59:46 +0600

    added countries list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CityList from "./components/CityList/CityList";
+import CountriesList from "./components/CountriesList/CountriesList";
 import AppLayout from "./pages/AppLayout";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
@@ -6,14 +10,53 @@ import PageNotFound from "./pages/PageNotFound";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 
+const URL = `http://localhost:3005`;
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("There was an error loading data....");
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchCities();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
+        <Route index element={<Homepage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
-        <Route path="app" element={<AppLayout />}></Route>
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={
+              <CityList cities={cities} isLoading={isLoading}></CityList>
+            }
+          />
+          <Route
+            path="cities"
+            element={
+              <CityList cities={cities} isLoading={isLoading}></CityList>
+            }
+          />
+          <Route
+            path="countries"
+            element={<CountriesList cities={cities} isLoading={isLoading} />}
+          ></Route>
+          <Route path="form" element={<p>Form</p>}></Route>
+        </Route>
         <Route path="login" element={<Login />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
---
+++ src/components/CountriesList/CountriesList.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+
+import CountryItem from "../Country/CountryItem";
+import Message from "../Message/Message";
+import Spinner from "../Spinner/Spinner";
+import styles from "./CountriesList.module.css";
+
+function CountriesList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city bt clicking on a city on the map" />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountriesList;
